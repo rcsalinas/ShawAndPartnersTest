f4bfdcf8db21d8c68b63bfbcb2a67f5f3021b164
Roberto Salinas <robertosalinasaguilar99@gmail.com>
2024-02-17 14:34:00 -0300

    feat: added csv holder

---
+++ csv-app-backend/src/data/csvData.ts
@@ -0,0 +1,14 @@
+export const csvData = [
+    {
+        name: 'John Doe',
+        city: 'New York',
+        country: 'USA',
+        favorite_sport: 'Basketball',
+    },
+    {
+        name: 'Jane Smith',
+        city: 'London',
+        country: 'UK',
+        favorite_sport: 'Football',
+    },
+]
